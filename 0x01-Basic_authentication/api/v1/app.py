43116cba39c753a7fc32334de873383b69f8b632
kweku-annan <kwekuannan.dev@gmail.com>
2024-11-15 22:09:52 +0000

    Add solution for task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,7 +16,10 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE')
-if auth_type == 'auth':
+if auth_type == 'basic_auth':
+    from api.v1.auth.auth import BasicAuth
+    auth = BasicAuth()
+else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -31,3 +31,8 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Returns None"""
         return None
+
+
+class BasicAuth(Auth):
+    """Performs the function of a basic authication"""
+    pass
