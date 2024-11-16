de0e3bb6cfb7bf3ffbc2775288819704995ef322
kweku-annan <kwekuannan.dev@gmail.com>
2024-11-16 22:52:39 +0000

    Add solution for task 101

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,13 +17,13 @@ class Auth:
             return True
 
         for excluded_path in excluded_paths:
-            if excluded_path.endswith('*'):
+            if excluded_path.endswith("*"):
                 if path.startswith(excluded_path[:-1]):
                     return False
             else:
                 if path == excluded_path:
                     return False
-        return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Returns None"""
