091e5e848758df317730fb5c8ea8c698b420d713
kweku-annan <kwekuannan.dev@gmail.com>
2024-11-17 02:34:00 +0000

    Add changes

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -59,8 +59,8 @@ def before_request():
         return
     if not auth.require_auth(request.path, path_list):
         return
-    if auth.authorization_header(request) is None\
-        and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
     if auth.authorization_header(request) is None:
         abort(401)
