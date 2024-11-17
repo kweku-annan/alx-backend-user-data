59ac1294d0485a5ea4ebf7cd16dced938e7232ab
kweku-annan <kwekuannan.dev@gmail.com>
2024-11-17 01:28:06 +0000

    Add solution for task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Implements the Session Authentication of this program"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Class for Session Authentication"""
+    pass
