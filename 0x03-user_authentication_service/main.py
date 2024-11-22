4fb2302dd5517cc83050b832d54175d5715b5097
kweku-annan <kwekuannan.dev@gmail.com>
2024-11-22 13:24:51 +0000

    Add changes

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,31 +1,15 @@
 #!/usr/bin/env python3
-"""Main file"""
-from user import User
+"""
+Main file
+"""
+
 from db import DB
-
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-
-
+from user import User
 
 my_db = DB()
 
-user = my_db.add_user("test@test.com", "PwdHashed")
-print(user.id)
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
 
-find_user = my_db.find_user_by(email="test@test.com")
-print(find_user.id)
-
-try:
-    find_user = my_db.find_user_by(email="test2@test.com")
-    print(find_user.id)
-except NoResultFound:
-    print("Not found")
-
-try:
-    find_user = my_db.find_user_by(no_email="test@test.com")
-    print(find_user.id)
-except InvalidRequestError:
-    print("Invalid")
-
-
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
