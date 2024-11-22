8cb5d0caacce5196efb6c40c999b20feaa0ad25a
kweku-annan <kwekuannan.dev@gmail.com>
2024-11-22 01:33:33 +0000

    Add solution for task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,6 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -32,3 +34,16 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+
+    def find_user_by(self, **kwargs):
+        """Returns a user object found in the user table"""
+        users = User()
+        table = users.__table__
+        column_names = [column.name for column in table.columns]
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
