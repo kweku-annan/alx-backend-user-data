b3315a2c43c9b973f58e53e2c30609f0ca79557a
kweku-annan <kwekuannan.dev@gmail.com>
2024-11-23 04:20:17 +0000

    Add solution for task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
-from crypt import methods
 
-from flask import Flask, jsonify, request
+from flask.helpers import make_response
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -25,6 +25,20 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """Implements the login function"""
+    data = request.form
+    email, password = data.get('email'), data.get('password')
+    valid_login = AUTH.valid_login(email, password)
+    if not valid_login:
+        abort(401)
+    response = make_response(jsonify(
+        {"email": email, "message": "logged in"}))
+    response.set_cookie('session_id', AUTH.create_session(email))
+    return response
 
 
 if __name__ == "__main__":
