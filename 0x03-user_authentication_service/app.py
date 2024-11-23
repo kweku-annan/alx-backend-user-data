88aa48c859472a87ee472d89e688a3bcb7a816dc
kweku-annan <kwekuannan.dev@gmail.com>
2024-11-23 11:45:27 +0000

    Add solution for task 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -53,5 +53,15 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile():
+    """Implements profile function"""
+    user_cookie = request.cookies.get('session_id', None)
+    user = AUTH.get_user_from_session_id(user_cookie)
+    if user_cookie is None or user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
