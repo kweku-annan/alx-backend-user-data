ca3713d5506b30afaff31116e36845058ffbb68d
kweku-annan <kwekuannan.dev@gmail.com>
2024-11-23 12:09:19 +0000

    Add solution for task 17

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,5 +63,17 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """Responds to password reset"""
+    data = request.form
+    email = data.get('email', None)
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset_token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
